71856af55c9dcdfb1eb17c23022af63341c0789e
CornaeX <nipitpon7753@gmail.com>
2026-02-28 00:55:27 +0700

    add a backend with tested api.

---
+++ backend/src/main/java/com/cnx/onyxbackend/OnyxbackendApplication.java
@@ -0,0 +1,13 @@
+package com.cnx.onyxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnyxbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnyxbackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cnx/onyxbackend/OnyxbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnx.onyxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnyxbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
